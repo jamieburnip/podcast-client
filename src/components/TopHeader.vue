6da4bef3080b271e67b6a8795f8aac87383830ec
Jamie Burnip <jamieburnip@gmail.com>
2017-04-30 01:12:40 +0100

    Create a basic header

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
-      // Header
+    <div class="container">
+      <p class="welcome">Oh, hi. <strong>Choose a podcast</strong>.</p>
+      <!-- <player></player> -->
+    </div>
   </header>
 </template>
 
@@ -14,3 +17,24 @@
     }
   }
 </script>
+
+<style lang="scss">
+  @import "~@/assets/sass/bootstrap";
+
+  header {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: $header-height;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding: $header-padding;
+    z-index: 2;
+
+    .welcome {
+      font-size: 1.1em;
+    }
+  }
+</style>
